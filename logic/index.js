2327c8611016c374b1c5e6f926af7d6fafd6b433
Bunningss <mahmudovi183@gmail.com>
2022-02-14 11:27:12 +0600

    14.02 - 11.00

---
+++ logic/index.js
@@ -0,0 +1,19 @@
+
+    const desktopToggle = document.querySelector('.toggle');
+    const mobileToggle = document.querySelector('.nav-mobile-toggle');
+    const mobileSidebar = document.querySelector('.sidebar');
+    const backdrop =document.querySelector('.backdrop');
+
+    desktopToggle && desktopToggle.addEventListener('click', () => {
+        mobileSidebar.classList.toggle('slideIn');
+    });
+    
+    mobileToggle && mobileToggle.addEventListener('click', () => {
+        mobileSidebar.classList.toggle('slideIn');
+    });
+    
+    backdrop && backdrop.addEventListener('click', ()=> {
+        mobileSidebar.classList.toggle('slideIn');
+    });
+
+    
