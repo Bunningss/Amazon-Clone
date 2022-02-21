488254bbd5d4a879b06b82f0f01792d2f47516fa
Bunningss <mahmudovi183@gmail.com>
2022-02-22 02:42:13 +0600

    22.02 - 2.42

--- logic/index.js
+++ logic/index.js
@@ -1,26 +1,38 @@
 
 
 // Sidebar
-    const desktopToggle = document.querySelector('.toggle');
-    const mobileToggle = document.querySelector('.nav-mobile-toggle');
-    const mobileSidebar = document.querySelector('.sidebar');
-    const backdrop =document.querySelector('.backdrop');
-    const body = document.body;
+const desktopToggle = document.querySelector('.toggle');
+const mobileSidebar = document.querySelector('.sidebar');
+const backdrop =document.querySelector('.backdrop');
+const body = document.body;
 
-    desktopToggle && desktopToggle.addEventListener('click', () => {
-        mobileSidebar.classList.toggle('slideIn');
-        body.classList.toggle('noscroll');
-    });
+desktopToggle && desktopToggle.addEventListener('click', () => {
+    mobileSidebar.classList.toggle('slideIn');
+    body.classList.toggle('noscroll');
+});
     
-    mobileToggle && mobileToggle.addEventListener('click', () => {
-        mobileSidebar.classList.toggle('slideIn');
-        body.classList.toggle('noscroll');
-    });
-    
-    backdrop && backdrop.addEventListener('click', () => {
-        mobileSidebar.classList.toggle('slideIn');
-        body.classList.toggle('noscroll');
-    });
+backdrop && backdrop.addEventListener('click', () => {
+    mobileSidebar.classList.toggle('slideIn');
+    body.classList.toggle('noscroll');
+});
+
+// Mobile Sidebar
+
+const mobileToggle = document.querySelector('.nav-mobile-toggle');
+const sidiebar = document.querySelector('.mobile-sidebar-bd');
+const mbackdrop = document.querySelector('.mobile-backdrop')
+
+mobileToggle.addEventListener('click', () => {
+    sidiebar.classList.toggle('m-slide-in');
+    body.classList.toggle('noscroll');
+});
+
+mbackdrop.addEventListener('click', () => {
+    sidiebar.classList.toggle('m-slide-in');
+    body.classList.toggle('noscroll');
+});
+
+
 
 // Nav account Flyout
 const navEle = document.querySelector('.account');
